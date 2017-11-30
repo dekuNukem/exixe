beea74f9a0ae664d8df96163ffa49966195353f9
dekunukem <dekunukem@gmail.com>
2017-11-30 15:06:15 +0000

    clean up temp file

--- push.sh
+++ push.sh
@@ -4,6 +4,7 @@ find . -type f -name "*.l#*" -exec rm -f {} \;
 find . -type f -name "*.csv#*" -exec rm -f {} \;
 find . -type f -name "*.DS_Store*" -exec rm -f {} \;
 find . -name "__pycache__" -exec rm -rf {} \;
+find . -type f -name "._*" -exec rm -f {} \;
 git add --all
 git commit -m "$@"
 git push origin master
